495fd6f39f73f03b3592e616fd9bf4488dc8a947
balakrishnan24 <balakrishnanrbe@gmail.com>
2019-10-03 17:40:57 +0530

    kochu

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,65 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-login',
+//   templateUrl: './login.component.html',
+//   styleUrls: ['./login.component.css']
+// })
+// export class LoginComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+// }
+
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({ templateUrl: 'login.component.html' })
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      accountType: ['', Validators.required]
+    });
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+    // this.router.navigateByUrl('/user');
+    if (this.loginForm.get('accountType').value == "Trainee") {
+      this.router.navigateByUrl('/user');
+    }
+    if (this.loginForm.get('accountType').value == "Trainer") {
+      this.router.navigateByUrl('/trainer');
+    }
+
+  }
+}
